2b123822620e1824d699110cd425ca6f246bfe6c
Graphic <graphic@SOD-SJH-516-10M.local>
2015-10-02 14:58:05 -0400

    need the words left

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
+var clickcount = 0;
+// var isSwitchOn = $(':button').hasClass('on')
 
+$(':button').on('click',function(e){
+	clickcount = clickcount +1;
+
+	var isSwitchOn = $(':button').hasClass('on')
+
+	if (isSwitchOn == true) {
+		$('body').removeClass('light');
+		$('body').addClass('dark');
+		$(':button').removeClass('on')
+		$('status').text("Hey, who turn off the lights?");
+		$('.status').css('color','white');
+
+	}
+	else if(isSwitchOn == false){
+		$('body').addClass('light');
+		$('body').removeClass('dark');
+		$(':button').addClass('on');
+		$('status').text("It's so bright in here!");
+		$('status').css('color','black');
+	}
+});
 
